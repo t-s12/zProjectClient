d59fb9ce26a05b5cff1a5f2062d4325d23781ece
Sowers <thomas.sowers.081@my.csun.edu>
2025-03-07 18:11:48 -0800

    class 3/7

---
+++ src/app/country-population.ts
@@ -0,0 +1,8 @@
+export interface CountryPopulation {
+    // yee haw bitches
+    id: number;
+    name: string;
+    iso2: string;
+    iso3: string;
+    population: number;
+}
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
-    RouterLink
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
